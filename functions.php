ee75ca805bc932ec918fb9d018de15f498018895
AwiZy63 <awizy0609@gmail.com>
2021-05-22 04:05:43 +0200

    Fix unused files imports

--- functions.php
+++ functions.php
@@ -5,8 +5,6 @@ add_theme_support('post-thumbnails');
 
 function add_theme_scripts() {
     wp_enqueue_style('style', get_stylesheet_uri());
-    wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css', ['style']);
-    wp_enqueue_style('main', get_template_directory_uri() . '/js/main.js', [], null, true);
 }
 
 function wpb_custom_new_menu() {
